af3afb801814973809170643d7cafdb6a9bd62d4
Lowkey <aneleuchiha000@gmail.com>
2020-03-10 10:13:57 +0200

    Python Data Types

---
+++ huhhh.py
@@ -0,0 +1,31 @@
+#declaring a string variable x
+x="Blue Ki Blast"
+#displaying the data type of the string variable x
+print(type(x))
+#extracting specific words
+print(x[2:5])
+num1=5
+num2=5.8
+print(type(num1))
+num3=7j
+num4=8j
+print(type(num3)), print(type(num4))
+#displaying the data type of the list
+myList=[23,82,42,2]
+print(type(myList))
+print(myList)
+#inserting numbers in the list
+myList.insert(18,25)
+print(myList)
+myList.reverse()
+print(myList)
+#sorting myList in ascending order
+myList.sort()
+print(myList)
+#checking the class of a data set with a curly bracket
+Cars={"Aston Martin", "VW", "Ford", "BMW", "Nissan"}
+print(type(Cars))
+#checking the class of a data set with a normal bracket
+Cars1=("Aston Martin", "VW", "Ford", "BMW", "Nissan")
+print(type(Cars1))
+
